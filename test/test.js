b823e331691ebf2d29433449835742b1dceee5df
alu0100851236 <alu0100851236@ull.edu.es>
2017-04-27 19:57:38 +0100

    Probando los cálculos del test

--- test/test.js
+++ test/test.js
@@ -5,8 +5,8 @@ let Shape = require('@ull-carlos-david/ull-shape');
 
 describe("getArea Triangle", function() {
   it("Se calcula el area del triangulo correctamente", function() {
-    let x = new Triangle({ width: 150, height: 150 });
+    let x = new Triangle({ width: 100, height: 100 });
     let result = x.getArea();
-     result.should.equal(11250);
+     result.should.equal(5000);
   })
 });
